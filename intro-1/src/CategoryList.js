3e5332b0717c953fa26416be9db78fef8cc7add8
Erdem Güzel <erdemguzel@Erdem-MacBook-Air.local>
2020-10-28 00:34:11 +0300

    Added new components into project

---
+++ intro-1/src/CategoryList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Categorylist extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Category list</h3>
+            </div>
+        )
+    }
+}
